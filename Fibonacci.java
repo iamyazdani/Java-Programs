23aa13ea404a543e21998c88714ed1533036a5e9
gulamyazdani <gulamyazdani03@gmail.com>
2024-04-12 00:15:34 +0530

    Fibonacci

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,9 +1,24 @@
 import java.util.Scanner;
 
 public class Fibonacci {
+    public static int printFibonacci(int num) {
+        if (num == 1 || num == 2) {
+            return 1;
+        } else {
+            int a = 1, b = 1, c = 0;
+            for (int i = 3; i <= num; i++) {
+                c = a + b;
+                a = b;
+                b = c;
+            }
+            return c;
+        }
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+        System.out.println(printFibonacci(num));
         sc.close();
     }
 }
