bcf135be2ee86e03e458b9a4f0c7bddd58669793
gulamyazdani <gulamyazdani03@gmail.com>
2024-04-11 21:22:52 +0530

    Fibonacci initialization

---
+++ Fibonacci.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        sc.close();
+    }
+}
