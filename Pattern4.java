ddbf68f0edaf363f4ec6303724b3a58044e862c4
gulamyazdani <gulamyazdani03@gmail.com>
2024-04-19 20:46:09 +0530

    Pattern4

---
+++ Pattern4.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Pattern4 {
+    public static void printPattern(int num) {
+        for (int i = 1; i <= num; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(i + " ");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+        printPattern(num);
+        sc.close();
+    }
+}
