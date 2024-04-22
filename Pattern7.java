904c739857519447414a6e21643d39e49b75e3a1
gulamyazdani <gulamyazdani03@gmail.com>
2024-04-22 21:19:21 +0530

    Pattern7

---
+++ Pattern7.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Pattern7 {
+    public static void printPattern(int n) {
+        
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+        printPattern(n);
+        sc.close();
+    }
+}
