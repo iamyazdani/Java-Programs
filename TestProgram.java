72846976864641809928a303ce9de6f684871737
gulamyazdani <gulamyazdani03@gmail.com>
2024-05-27 21:45:58 +0530

    Test Program Modified

--- TestProgram.java
+++ TestProgram.java
@@ -1,6 +1,10 @@
 public class TestProgram {
     public static void main(String[] args) {
-        int num = 0b101;
-        System.out.println(num);
+        int num1 = 5;
+        int num2 = 8;
+        int num3 = 9;
+        // int num4 = 3;
+        boolean result = num1 < num2 && num2 > num3 || num3 == 9;
+        System.out.println(!result && true);
     }
 }
