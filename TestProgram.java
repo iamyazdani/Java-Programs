f6c301c9493f7cb43a0b5722a3e91c032c7bfc2a
gulamyazdani <gulamyazdani03@gmail.com>
2024-05-29 19:59:08 +0530

    Test Program Modified

--- TestProgram.java
+++ TestProgram.java
@@ -1,10 +1,11 @@
+import java.util.Scanner;
+
 public class TestProgram {
     public static void main(String[] args) {
-        int num1 = 5;
-        int num2 = 8;
-        int num3 = 9;
-        // int num4 = 3;
-        boolean result = num1 < num2 && num2 > num3 || num3 == 9;
-        System.out.println(!result && true);
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+        System.out.println((num % 2 == 0) ? "Number is Even" : "Number is Odd");
+        sc.close();
     }
 }
