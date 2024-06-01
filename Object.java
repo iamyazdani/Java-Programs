6a323e87ed14a35b1ef60a370bdf4e4ff6337b0d
gulamyazdani <gulamyazdani03@gmail.com>
2024-06-01 19:56:39 +0530

    Added Object.java

---
+++ Object.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+class Computer {
+    public int add(int num1, int num2) {
+        return num1 + num2;
+    }
+}
+
+public class Object {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        int num1 = sc.nextInt();
+        System.out.print("Enter second number: ");
+        int num2 = sc.nextInt();
+        Computer comp = new Computer();
+        int result = comp.add(num1, num2);
+        System.out.println("Sum of the two numbers: " + result);
+        sc.close();
+    }
+}
