910c09c9da35a89b34ea80a9e75ec27cc18ecde7
gulamyazdani <gulamyazdani03@gmail.com>
2024-06-05 23:53:07 +0530

    Encapsulation.java Modified

--- Encapsulation.java
+++ Encapsulation.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 class Human {
     private int age;
     private String name;
@@ -14,14 +16,14 @@ class Human {
 
 public class Encapsulation {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String name = sc.nextLine();
+        System.out.print("Enter your age in yrs: ");
+        int age = sc.nextInt();
         Human human = new Human();
-        // human.name = "Gulam";
-        // human.age = 24;
-        // System.out.println("Your name is: " + human.name.concat(" Yazdani"));
-        // System.out.println("Your age is: " + human.age);
-        
-        human.getAge(24);
-        System.out.println("Your name is: " + human.getName("Gulam").concat(" Yazdani"));
-        System.out.println("Your age is: " + human.getAge(24));        
+        System.out.println("Your name is: " + human.getName(name));
+        System.out.println("You are " + human.getAge(age) + " years old");
+        sc.close();        
     }
 }
