27461de07ae89564ca171b62bbfbb108623ff766
gulamyazdani <gulamyazdani03@gmail.com>
2024-06-29 20:14:24 +0530

    C2M Practice

---
+++ CustomerToMeter.java
@@ -0,0 +1,15 @@
+/**
+ * InnerCustomer
+*/
+class Person {
+    String personName;
+}
+
+public class CustomerToMeter {
+    public static void main(String[] args) {
+        String firstName = "Gulam ";
+        String lastName = "Yazdani";
+        String fullName =firstName.concat(lastName);
+        System.out.print("Your Name is: " + fullName);
+    }
+}
